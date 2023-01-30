a7a563ca0ed8b54988eb9a511b170db54b0a1b2a
bannwafa <fabian_bannwart@outlook.com>
2023-01-30 15:30:09 +0100

    Assinment 2 fast fertig

---
+++ Assignment 2/Projekt/Logik/calc.js
@@ -0,0 +1,28 @@
+function berechne() {
+    var einnahmen = 0;
+    var ausgaben = 0;
+
+    var einnahmenElements = document.getElementsByClassName("einnahmen");
+    var ausgabenElements = document.getElementsByClassName("ausgaben");
+
+    for (let i = 0; i < einnahmenElements.length; i++) {
+        if (parseInt(einnahmenElements[i].value) > 0) {
+            einnahmen += parseInt(einnahmenElements[i].value);
+        }
+    }
+
+    for (let i = 0; i < ausgabenElements.length; i++) {
+        if (parseInt(ausgabenElements[i].value) > 0) {
+            ausgaben += parseInt(ausgabenElements[i].value);
+        }
+    }
+
+    var result = einnahmen - ausgaben;
+
+    document.getElementById("resultat").innerHTML = result.toFixed(2);
+}
+
+function reset() {
+    debugger;
+    document.getElementById("resultat").innerHTML = '123';
+}
